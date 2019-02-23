c670a385dad098bea0749ed3f724eb0df401044d
Christian Carey <christian.carey@gmail.com>
2019-02-23 12:35:25 -0700

    add retention to prometheus startup

--- scripts/install-prometheus.sh
+++ scripts/install-prometheus.sh
@@ -96,6 +96,7 @@ Type=simple
 ExecStart=/usr/local/bin/prometheus \
     --config.file /etc/prometheus/prometheus.yml \
     --storage.tsdb.path /var/lib/prometheus/ \
+    --storage.tsdb.retention 90d \
     --web.console.templates=/etc/prometheus/consoles \
     --web.console.libraries=/etc/prometheus/console_libraries
 
